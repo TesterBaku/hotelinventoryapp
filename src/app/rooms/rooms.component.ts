908a5181e73afd7b3691f210b549026cdfb21d3f
Rufat Malikov <testerbaku@yahoo.com>
2022-10-28 01:00:20 -0700

    3:41:06

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Room } from './rooms';
+
+@Component({
+  selector: 'hinv-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent implements OnInit {
+
+  hotelName = 'Hilton Hotels';
+
+  numberOfRooms = 10;
+
+  hideRooms = false;
+
+  rooms: Room = {
+
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5
+  }
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
+
+}
